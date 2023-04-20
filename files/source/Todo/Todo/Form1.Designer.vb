0f4a5723e97cf35960173531c93fd5c40e1faef4
Csani10 <csanadmozner@gmail.com>
2023-04-20 18:04:29 +0200

    w

--- files/source/Todo/Todo/Form1.Designer.vb
+++ files/source/Todo/Todo/Form1.Designer.vb
@@ -22,6 +22,7 @@ Partial Class Csinálnivaló
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Csinálnivaló))
         Me.CheckedListBox1 = New System.Windows.Forms.CheckedListBox()
         Me.Button1 = New System.Windows.Forms.Button()
         Me.TextBox2 = New System.Windows.Forms.TextBox()
@@ -43,7 +44,7 @@ Partial Class Csinálnivaló
         Me.Button1.Name = "Button1"
         Me.Button1.Size = New System.Drawing.Size(152, 59)
         Me.Button1.TabIndex = 1
-        Me.Button1.Text = "Hozzáad"
+        Me.Button1.Text = "Add todo"
         Me.Button1.UseVisualStyleBackColor = True
         '
         'TextBox2
@@ -60,7 +61,7 @@ Partial Class Csinálnivaló
         Me.Button2.Name = "Button2"
         Me.Button2.Size = New System.Drawing.Size(148, 50)
         Me.Button2.TabIndex = 4
-        Me.Button2.Text = "Jelöltek törlése"
+        Me.Button2.Text = "Delete selected"
         Me.Button2.UseVisualStyleBackColor = True
         '
         'Csinálnivaló
@@ -72,8 +73,9 @@ Partial Class Csinálnivaló
         Me.Controls.Add(Me.TextBox2)
         Me.Controls.Add(Me.Button1)
         Me.Controls.Add(Me.CheckedListBox1)
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
         Me.Name = "Csinálnivaló"
-        Me.Text = "Csinálnivaló"
+        Me.Text = "Todo"
         Me.ResumeLayout(False)
         Me.PerformLayout()
 
